35a7af49b6093f707a528c488d6802c568bf89ee
Jesus Verduzco <jesus@verduzco.me>
2023-03-13 18:08:34 -0600

    Add OP image

--- pages/index.vue
+++ pages/index.vue
@@ -11,8 +11,8 @@
       <Meta property="og:description" content="This is a Nuxt 3 project with PrimeVue + Eslint + Stylelint" />
       <Meta property="og:url" content="https://www.verduzco.me/" />
       <Meta property="og:type" content="website" />
-      <Meta property="og:image" content="" />
-      <Meta property="og:image:secure_url" content="" />
+      <Meta property="og:image" content="https://raw.githubusercontent.com/jeverduzco/nuxt-starter-template/main/public/capture.jpeg" />
+      <Meta property="og:image:secure_url" content="https://raw.githubusercontent.com/jeverduzco/nuxt-starter-template/main/public/capture.jpeg" />
       <Meta property="og:image:type" content="image/jpeg" />
       <Meta property="og:image:width" content="1200" />
       <Meta property="og:image:height" content="630" />
@@ -22,7 +22,7 @@
       <Meta name="twitter:card" content="summary_large_image" />
       <Meta name="twitter:title" content="Nuxt 3 - Starter Template" />
       <Meta name="twitter:description" content="This is a Nuxt 3 project with PrimeVue + Eslint + Stylelint" />
-      <Meta name="twitter:image" content="" />
+      <Meta name="twitter:image" content="https://raw.githubusercontent.com/jeverduzco/nuxt-starter-template/main/public/capture.jpeg" />
     </Head>
     <PageContent />
   </div>
